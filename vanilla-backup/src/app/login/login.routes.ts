21b7a447609aee4da71f22e4d25f1453829a920e
hantsy <hantsy@gmail.com>
2016-10-07 02:42:13 +0800

    remove backup folder.

--- vanilla-backup/src/app/login/login.routes.ts
+++
@@ -1,6 +0,0 @@
-import { LoginComponent } from './login.component';
-import { Routes } from '@angular/router';
-
-export const loginRoutes: Routes = [
-  { path: 'login', component: LoginComponent }
-];
