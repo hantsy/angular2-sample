cc1f4bb3c85c6adaf9eaa9988f70b7edc7e5d3e3
hantsy <hantsy@gmail.com>
2017-01-08 16:43:54 +0800

    updated to angular-cli 1.0-beta.24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http } from '@angular/http';
-import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { Ng2BootstrapModule } from 'ng2-bootstrap';
 
 
 import { AppRoutingModule } from './app-routing.module';
